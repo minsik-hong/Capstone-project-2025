0dd58ef7abcb907c50ac8272295cc6ebda6bfedd
miniDKU <32200511@dankook.ac.kr>
2025-03-23 08:05:19 +0900

    Chore: 불필요한 테스트 코드 삭제

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
-console.log('API_URL:', API_URL);
 
 // 회원가입
 export const registerUser = async (userData) => {
