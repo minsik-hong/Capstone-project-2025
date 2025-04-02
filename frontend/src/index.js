4df5a737cd2d23e834fdfdfcd7d49b5a55aeab8b
minsikhonggg <andrew.minsik.hong@gmail.com>
2025-04-02 15:22:02 +0900

    up

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // ReactDOM을 사용하여 React 컴포넌트를 DOM에 렌더링
 import App from './App'; // App 컴포넌트 가져오기
-import './styles/chatbot.css'; // 스타일 시트 가져오기
 
 // React 애플리케이션 렌더링
 const root = ReactDOM.createRoot(document.getElementById('root'));
